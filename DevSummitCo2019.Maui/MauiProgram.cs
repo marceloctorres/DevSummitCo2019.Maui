4653ab3fb374e4de67a779b447da811f93d37c6c
Marcelo Cesar Torres <mtorres@esri.co>
2024-05-30 19:58:57 -0500

    Add User Secrets

--- DevSummitCo2019.Maui/MauiProgram.cs
+++ DevSummitCo2019.Maui/MauiProgram.cs
@@ -5,6 +5,9 @@ using Esri.ArcGISRuntime;
 using Esri.ArcGISRuntime.Maui;
 
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
 
 namespace DevSummitCo2019.Maui
 {
@@ -13,19 +16,21 @@ namespace DevSummitCo2019.Maui
     public static MauiApp CreateMauiApp()
     {
       var builder = MauiApp.CreateBuilder();
+      var config = new ConfigurationBuilder()
+        .AddUserSecrets(Assembly.GetExecutingAssembly())
+        .Build();
+      var ARCGIS_API_KEY = config["ArcGISRuntime:ApiKey"];
+
       builder
         .UseMauiApp<App>()
-        .UseArcGISRuntime(config => config.UseApiKey("AAPKc5e8d836cf334edd9063202b7a6c116diWf_PrnwqMwic9YnQGvcD-ZR09TKiYFW4qgKLLmOrgX3oHQXTeWGcsWS-A-5azVW"))
-        .UsePrism(prism =>
-        {
-          prism
+        .UseArcGISRuntime(config => config.UseApiKey(ARCGIS_API_KEY))
+        .UsePrism(prism => prism
             .RegisterTypes(registry =>
             {
               registry.RegisterForNavigation<NavigationPage>();
               registry.RegisterForNavigation<MainPage, MainPageViewModel>();
             })
-            .CreateWindow(async navigationService => await navigationService.NavigateAsync("NavigationPage/MainPage"));
-        })
+            .CreateWindow(async navigationService => await navigationService.NavigateAsync("NavigationPage/MainPage")))
         .ConfigureFonts(fonts =>
         {
           fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
